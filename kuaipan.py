853533ec86a89c5e9176ed7daa63eb7b015d274b
root <root@localhost.localdomain>
2013-07-16 11:33:52 +0800

    kuaipan2 is ok

--- kuaipan.py
+++ kuaipan.py
@@ -7,13 +7,16 @@ import urllib
 import cookielib
 import chardet
 
-class Verypsp(object):
+reload(sys) 
+sys.setdefaultencoding('utf-8')
+
+class Kuaipan(object):
     
     def __init__(self):
         self.name = self.pwd = self.content = ''
         self.cj = cookielib.LWPCookieJar()
         try:
-            self.cj.revert('verypsp.cookie')
+            self.cj.revert('kuaipan.cookie')
         except Exception,e:
             print e
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
@@ -25,27 +28,38 @@ class Verypsp(object):
         self.pwd = password
         
     def login(self):
-        '''verypsp'''
-        params = {'username':self.name, 'password':self.pwd}
+        '''kuaipan login'''
+        params = {'username':self.name, 'userpwd':self.pwd,'isajax':'yes'}
         print 'login.......'
+        user_agent ='Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20130514 Firefox/17.0'
+        headers = { 'User-Agent' : user_agent }
+        
         req = urllib2.Request(
-            'http://bbs.verypsp.com/logging.php?action=login&loginsubmit=yes',
-            urllib.urlencode(params)
+            'https://www.kuaipan.cn/index.php?ac=account&op=login',
+            urllib.urlencode(params),
+            headers
         )
         self.operate = self.opener.open(req)
         #print self.operate.geturl()
         rawdata = self.operate.read()
         #print chardet.detect(rawdata)['encoding']
         #rawdata = rawdata.decode(chardet.detect(rawdata)['encoding'])
-        rawdata = rawdata.decode('gbk')
+        rawdata = rawdata.decode('utf8')
         print rawdata
         if rawdata.find("登录失败，您还可以尝试") < 0 :
             print 'Logged on successfully!'
-            self.cj.save('verypsp.cookie')
-            #self.sign()
+            self.cj.save('kuaipan.cookie')
+            #self.getindex()
         else:
-            print 'Logged on error'        
-verypsp = Verypsp()
+            print 'Logged on error'    
+            
+    def getindex(self):
+        '''get index'''
+        req = urllib2.Request("http://www.kuaipan.cn/index.php?ac=home&op=dir&withdel=0&fileId=0")
+        self.operate = self.opener.open(req)
+        rawdata = self.operate.read()     
+        print rawdata   
+kuaipan = Kuaipan()
 import mypasswd
-verypsp.setinfo(mypasswd.VERYPSP_USERNAME,mypasswd.VERYPSP_PASSWD)
-verypsp.login()
+kuaipan.setinfo(mypasswd.KUAIPAN_USERNAME,mypasswd.KUAIPAN_PASSWD)
+kuaipan.login()
---
+++ kuaipan2.py
@@ -0,0 +1,62 @@
+__author__ = 'clownfish'
+#coding:utf-8
+import urllib2,urllib,cookielib,json
+username = "快盘用户名"
+password = "快盘密码"
+class sign(object):
+    username = ''
+    password = ''
+    #登录显示页面
+    indexurl = 'https://www.kuaipan.cn/account_login.htm'
+    #登录的form表单url
+    loginurl = 'https://www.kuaipan.cn/index.php?ac=account&op=login'
+    #签到的真正url
+    signurl = 'http://www.kuaipan.cn/index.php?ac=common&op=usersign'
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+    def login(self):
+        cj = cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        urllib2.install_opener(opener)
+        print "打开登录页面"
+        try:
+            urllib2.urlopen(self.indexurl)
+            post_data = {'username':self.username,'userpwd':self.password,'isajax':'yes'}
+            req=urllib2.Request(self.loginurl,urllib.urlencode(post_data))
+        except Exception, e:
+            print "网络链接错误"
+            return False
+        print "登录成功,准备签到！"
+        response = urllib2.urlopen(req)
+        login=response.read()
+        return login
+    def sign(self):
+        response = urllib2.urlopen(self.signurl)
+        sign = response.read()
+        l = json.loads(sign)
+        if (l and l['state'] == 1) or \
+        (l and 0 == l['state'] and l['increase'] * 1 == 0 and l['monthtask'].M900 == 900):
+            print "恭喜你签到成功！"
+            k = l['increase']*1
+            m = l['rewardsize'] * 1
+            if (k == 0 and l['monthtask'].M900 == 900):
+                print "本月签到积分已领取完成"
+            else:
+                print "签到奖励积分:%s" % (k)
+            if m == 0:
+                print "手气太不好了！奖励 0M 空间"
+            else:
+                print "签到奖励空间：%s" % (m)
+        else:
+            if (l['state'] == -102):
+                print "今天您已经签到过了"
+            else:
+                print "签到失败，遇到网络错误，请稍后再试！"
+        return sign
+
+import mypasswd
+if __name__ == "__main__": 
+    sign = sign(mypasswd.KUAIPAN_USERNAME,mypasswd.KUAIPAN_PASSWD)
+    if sign.login():
+        sign.sign()
