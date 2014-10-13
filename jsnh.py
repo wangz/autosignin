1c7fb4d9f57222bc3413ab6a9a6293ab8345215e
wangz <wangzhuo1987@gmail.com>
2014-10-13 23:00:27 +0800

    jsnh ok

--- jsnh.py
+++ jsnh.py
@@ -45,12 +45,12 @@ class Moofeel(object):
         if rawdata.find("登录失败，你还可以尝试") < 0 :
             print 'Logged on successfully!'
             self.cj.save('jsnh.cookie')
-            #self.sign()
+            self.sign()
         else:
             print 'Logged on error'
     def sign(self):
         '''找到最新的领取MB的地址'''
-        siteurl="http://www.moofeel.com/forum-96-1.html"
+        siteurl="http://www.jsnh.info/forum.php?mod=forumdisplay&fid=36"
         isfind = False
         count = 0
         while(isfind == False and count<=300):
@@ -59,28 +59,33 @@ class Moofeel(object):
             req = urllib2.Request(siteurl)
             self.operate = self.opener.open(req)
             rawdata = self.operate.read()
-            #rawdata = rawdata.decode(chardet.detect(rawdata)['encoding'])
-            rawdata = rawdata.decode("gbk")
+            rawdata = rawdata.decode(chardet.detect(rawdata)['encoding'])
+            #print rawdata
+            #rawdata = rawdata.decode("gbk")
             for m in BeautifulSoup(rawdata).findAll(attrs={'class' : re.compile("new")}):
-                e = m.find(attrs={'class' : re.compile("xst")})
-            if(e!=None):
-                pattern = str(time.localtime()[0])+".*"+str(time.localtime()[1]) +".*"+ str(time.localtime()[2])
-                if(len(re.findall(pattern,str(e)))>0):
-                    realUrl = e.get('href')
-                    isfind = True
-                else:
-                    time.sleep(0.2)
+                e = m.find(attrs={'class' : re.compile("s xst")})
+                if(e!=None):
+                    print e
+                    pattern = str(time.localtime()[0])+".*"+str(time.localtime()[1]) +".*"+ str(time.localtime()[2])
+                    print pattern
+                    time.sleep(1)
+                    if(len(re.findall(pattern,str(e)))>0):
+                        realUrl = e.get('href')
+                        isfind = True
+                        break
+                    else:
+                        time.sleep(0.2)
             else:
                 continue
         print realUrl
-        self.replyAndFetch(realUrl)
+        self.replyAndFetch("http://www.jsnh.info/" + realUrl)
     def replyAndFetch(self,realUrl):
-        '''回复信息并领取MB'''
+        '''回复信息'''
         req = urllib2.Request(realUrl)
         self.operate = self.opener.open(req)
         rawdata = self.operate.read()
-        #rawdata = rawdata.decode(chardet.detect(rawdata)['encoding'])
-        rawdata = rawdata.decode('gbk')
+        rawdata = rawdata.decode(chardet.detect(rawdata)['encoding'])
+        #rawdata = rawdata.decode('gbk')
         #获取formhash
         formhash = None
         for m in BeautifulSoup(rawdata).findAll('input'):
@@ -92,27 +97,18 @@ class Moofeel(object):
         for m in BeautifulSoup(rawdata).findAll('form'):
             if(m.get('id')=='fastpostform'):
                 form_action = m.get('action')
+        form_action = 'http://www.jsnh.info/'+form_action+'&inajax=1'
         print form_action
-        #获取领取MB地址
-        fetchmb_url = None
-        for m in BeautifulSoup(rawdata).findAll('a'):
-            for x in m.findAll('img'):
-                if(x.get('src').find('signin_reply')>0):
-                    fetchmb_url = m.get('href')
-        print "fetchmb_url" + str(fetchmb_url)
         #回复信息
-        params = {'message':'qiandao qiandao youjiang youjiang ~', 'formhash':formhash,'subject':''}
+        params = {'message':'shao xiang bao you ping an ~', 'formhash':formhash,'subject':'','usesig':''}
         print 'reply.......'
         req = urllib2.Request(
-            'http://www.moofeel.com/'+form_action,
+            form_action,
             urllib.urlencode(params)
         )
         self.operate = self.opener.open(req)
         rawdata = self.operate.read()
-        #点击领取MB
-        self.operate = urllib2.urlopen(fetchmb_url)
-        rawdata = self.operate.read()
-        print rawdata.decode('gbk')
+        print rawdata
 moofeel = Moofeel()
 import mypasswd
 moofeel.setinfo(mypasswd.JSNH_USERNAME,mypasswd.JSNH_PASSWD)
